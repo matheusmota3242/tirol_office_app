d5ce72729c019f8018a1cf3fd1a65dcd1b241aa8
matheus.mota <matheus.mota@imd.ufrn.br>
2021-01-11 10:10:22 -0300

    pagina de registro

--- lib/views/screens/auth/register_view.dart
+++ lib/views/screens/auth/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:tirol_office_app/views/screens/auth/register_view.dart';
 
-class LoginView extends StatelessWidget {
+class RegisterView extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   static const double _horizontalPadding = 50.0;
   @override
@@ -11,25 +10,24 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       body: Container(
         child: Form(
-          key: _formKey,
           child: ListView(
             children: [
-              _loginImage(screenHeight),
+              _registerImage(screenHeight),
               _titlePage(screenHeight),
               _usernameField(screenHeight),
+              _cpfField(screenHeight),
               _passwordField(screenHeight),
-              _forgotPasswordButton(screenHeight),
-              _loginButton(context, screenHeight),
-              _orText(),
+              _confirPasswordField(screenHeight),
               _registerButton(context, screenHeight)
             ],
           ),
+          key: _formKey,
         ),
       ),
     );
   }
 
-  Widget _loginImage(double screenHeight) {
+  Widget _registerImage(double screenHeight) {
     var heightImage = 150.0;
     var paddingTop = (screenHeight / 12);
     var paddingBottom = (screenHeight / 30);
@@ -37,7 +35,7 @@ class LoginView extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.only(top: paddingTop, bottom: paddingBottom),
         child: SvgPicture.asset(
-          'assets/images/login.svg',
+          'assets/images/register.svg',
           height: heightImage,
         ),
       ),
@@ -51,7 +49,7 @@ class LoginView extends StatelessWidget {
       padding: EdgeInsets.only(bottom: paddingBottom),
       child: Center(
         child: Text(
-          'Login',
+          'Registro',
           style: TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.w700,
@@ -68,14 +66,45 @@ class LoginView extends StatelessWidget {
     var fieldLeftPadding = 8.0;
     var fieldRightPadding = 0.0;
     return Padding(
-      padding: EdgeInsets.fromLTRB(_horizontalPadding, verticalPadding,
-          _horizontalPadding, verticalPadding),
+      padding: EdgeInsets.fromLTRB(
+        _horizontalPadding,
+        verticalPadding,
+        _horizontalPadding,
+        (verticalPadding / 2),
+      ),
       child: TextFormField(
         decoration: InputDecoration(
           filled: true,
           contentPadding: EdgeInsets.fromLTRB(fieldLeftPadding,
               fieldVerticalPadding, fieldRightPadding, fieldVerticalPadding),
-          hintText: 'Usuário',
+          hintText: 'E-mail',
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cpfField(double screenHeight) {
+    var borderWidth = 1.0;
+    var verticalPadding = (screenHeight / 40);
+    var fieldVerticalPadding = 8.0;
+    var fieldLeftPadding = 8.0;
+    var fieldRightPadding = 0.0;
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        _horizontalPadding,
+        verticalPadding,
+        _horizontalPadding,
+        (verticalPadding / 2),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          filled: true,
+          contentPadding: EdgeInsets.fromLTRB(fieldLeftPadding,
+              fieldVerticalPadding, fieldRightPadding, fieldVerticalPadding),
+          hintText: 'CPF',
           border: OutlineInputBorder(
             borderSide: BorderSide.none,
           ),
@@ -91,7 +120,11 @@ class LoginView extends StatelessWidget {
     var fieldRightPadding = 0.0;
     return Padding(
       padding: EdgeInsets.fromLTRB(
-          _horizontalPadding, verticalPadding, _horizontalPadding, 0.0),
+        _horizontalPadding,
+        verticalPadding,
+        _horizontalPadding,
+        (verticalPadding / 2),
+      ),
       child: Column(
         children: [
           TextFormField(
@@ -114,72 +147,56 @@ class LoginView extends StatelessWidget {
     );
   }
 
-  Widget _forgotPasswordButton(double screenHeight) {
-    return Container(
-      height: 32.0,
-      padding: EdgeInsets.only(right: _horizontalPadding),
-      alignment: Alignment.topRight,
-      child: TextButton(
-        onPressed: () {},
-        child: Text(
-          'Esqueceu a senha?',
-          style: TextStyle(fontSize: 12),
-        ),
-      ),
-    );
-  }
-
-  Widget _loginButton(BuildContext context, double screenHeight) {
-    var buttonPadding = 16.0;
+  Widget _confirPasswordField(double screenHeight) {
     var verticalPadding = (screenHeight / 40);
+    var fieldVerticalPadding = 8.0;
+    var fieldLeftPadding = 8.0;
+    var fieldRightPadding = 0.0;
     return Padding(
-      padding: EdgeInsets.fromLTRB(_horizontalPadding, verticalPadding,
-          _horizontalPadding, verticalPadding),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        onPressed: () {},
-        child: Text(
-          'Entrar',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        color: Theme.of(context).buttonColor,
-        padding: EdgeInsets.only(top: buttonPadding, bottom: buttonPadding),
+      padding: EdgeInsets.fromLTRB(
+        _horizontalPadding,
+        verticalPadding,
+        _horizontalPadding,
+        (verticalPadding / 2),
       ),
-    );
-  }
-
-  Widget _orText() {
-    var verticalPadding = 10.0;
-    return Container(
-      padding: EdgeInsets.only(top: verticalPadding, bottom: verticalPadding),
-      alignment: Alignment.center,
-      child: Text(
-        'ou',
-        style: TextStyle(color: Colors.black54),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              counterStyle: TextStyle(color: Colors.red),
+              hintText: 'Confirmar senha',
+              contentPadding: EdgeInsets.fromLTRB(
+                  fieldLeftPadding,
+                  fieldVerticalPadding,
+                  fieldRightPadding,
+                  fieldVerticalPadding),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _registerButton(BuildContext context, double screenHeight) {
-    var verticalPadding = (screenHeight / 40);
     var buttonPadding = 16.0;
+    var verticalPadding = (screenHeight / 40);
     return Padding(
       padding: EdgeInsets.fromLTRB(_horizontalPadding, verticalPadding,
           _horizontalPadding, verticalPadding),
       child: RaisedButton(
-        onPressed: () => Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => RegisterView(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        onPressed: () {},
+        child: Text(
+          'Enviar',
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        color: Colors.grey[200],
-        child: Text(
-          'Registre-se',
-          style: TextStyle(color: Theme.of(context).buttonColor),
-        ),
+        color: Theme.of(context).buttonColor,
         padding: EdgeInsets.only(top: buttonPadding, bottom: buttonPadding),
       ),
     );
